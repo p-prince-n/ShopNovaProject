db82b577a93966b7ebad458529d0c7a99ac99695
p-prince-n <pp2358296@gmail.com>
2025-09-19 17:07:13 +0530

    uploading

--- server/utils/generateTokenAndSetCookie.js
+++ server/utils/generateTokenAndSetCookie.js
@@ -7,7 +7,7 @@ export const generateTokenAndSetCookie=(res, userId)=>{
     res.cookie("jwt", token, {
         httpOnly: true,
         secure:process.env.NODE_ENV === 'production',
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000,
     })
     return token
