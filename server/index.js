cfc3f089afcdc2d5453e6071c48534a63664472a
p-prince-n <pp2358296@gmail.com>
2025-11-25 15:08:48 +0530

    changing url

--- server/index.js
+++ server/index.js
@@ -21,7 +21,7 @@ const app= express();
 dotenv.config();
 app.use(express.json());
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://shopnovaproject-2.onrender.com',
     methods:['GET','PUT','POST','DELETE', 'PATCH'],
     credentials: true,
 }))
